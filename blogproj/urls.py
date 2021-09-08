0ca2e56bf5ae2e32953f873f2f658afad94daa0a
Noam Blanks <noamblanks@gmail.com>
2021-09-08 12:42:03 -0400

    heroku prep

---
+++ blogproj/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from blog.views import BlogViewSet
+
+# create a new router
+router = routers.DefaultRouter()
+# register our viewsets
+router.register(r'blog', BlogViewSet) #register "/blog" routes
+
+
+urlpatterns = [
+    # add all of our router urls
+    path("", include(router.urls))
+]
